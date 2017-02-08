b1b75a0dc4ddc373ec926a7fabefd770118f906e
markpritchett <markpritchett@hotmail.co.uk>
2017-02-08 13:53:36 +0000

    Added validation to create account

--- ui/src/containers/NewAccountDialog.js
+++ ui/src/containers/NewAccountDialog.js
@@ -1,23 +1,68 @@
-
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Dialog from 'material-ui/Dialog'
 import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import formatMoney from '../formatMoney'
 import { createAccount, hideNewAccountForm } from '../actions'
+
 class NewAccountDialog extends Component {
-
-
-    componentDidMount() {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            accountNameErrorMessage: null,
+            openingBalanceErrorMessage: null
+        };
     }
 
     submitRequest() {
-        const { dispatch } = this.props
+        const { dispatch, accounts } = this.props
 
-        let accountName = this.refs.accountName.input.value
-        let openingBalance = this.refs.openingBalance.input.value
+        let accountName = this.refs.accountName.input.value.trim()
+        let openingBalance = this.refs.openingBalance.input.value.trim()
+
+        let hasErrors = this.hasErrors(accounts, accountName, openingBalance)
+
+        if (hasErrors) {
+            return
+        }
+
         dispatch(createAccount(accountName, openingBalance))
+    }
+
+    hasErrors(accounts, accountName, openingBalance) {
+        this.setState({ accountNameErrorMessage: null })
+        this.setState({ openingBalanceErrorMessage: null })
+
+        let hasErrors = false
+        
+        if (!accountName) {
+            this.setState({ accountNameErrorMessage: 'Account Name is required' })
+            hasErrors = true
+        } else if (accounts.find(account => account.name.toLowerCase() === accountName.toLowerCase())) {
+            this.setState({ accountNameErrorMessage: `Account Name ${accountName} already exists!` })
+            hasErrors = true
+        }
+
+        if (!openingBalance) {
+            this.setState({ openingBalanceErrorMessage: 'Opening Balance is required' })
+            hasErrors = true
+        } else {
+            openingBalance = parseFloat(openingBalance)
+
+            if (Number.isNaN(openingBalance)) {
+                this.setState({ openingBalanceErrorMessage: 'Opening Balance must be a number' })
+                hasErrors = true
+            } else if (openingBalance < 0.01) {
+                this.setState({ openingBalanceErrorMessage: `Opening Balance cannot be less that ${formatMoney(0.01)}` })
+                hasErrors = true
+            } else if (openingBalance > 1000.00) {
+                this.setState({ openingBalanceErrorMessage: `Jeez, I know this is a fake app, but we can't give out more than ${formatMoney(1000.00)}` })
+                hasErrors = true
+            }
+        }
+
+        return hasErrors
     }
 
     cancel() {
@@ -50,8 +95,12 @@ class NewAccountDialog extends Component {
                 open={showNewAccountForm}
                 onRequestClose={() => this.cancel()}
             >
-                <TextField ref="accountName" hintText="Account name" />
-                <TextField ref="openingBalance" hintText="Opening Balance" />
+                <div>
+                    <TextField ref="accountName" hintText="Account name" errorText={this.state.accountNameErrorMessage} />
+                </div>
+                <div>
+                    <TextField ref="openingBalance" hintText="Opening Balance" errorText={this.state.openingBalanceErrorMessage} />
+                </div>
             </Dialog>
         );
     }
@@ -60,6 +109,7 @@ class NewAccountDialog extends Component {
 const mapStateToProps = state => {
     const { accounts } = state
     return {
+        accounts: accounts.items,
         showNewAccountForm: accounts.showNewAccountForm
     }
 }
